720dbf0ec6f72b0007afd5c7345a1c027fa84d3e
Md.Sami-El Bashar Rhythm <md.sami-elbasharrhythm@MdSami-Els-MacBook-Air-2.local>
2026-08-27 13:52:31 +0600

    testing3

--- index.js
+++ index.js
@@ -127,7 +127,6 @@ app.get(
 app.get(
   "/auth/google/callback",
   passport.authenticate("google", {
-    failureRedirect: `${process.env.FRONTEND_URL || "http://localhost:5173"}/login?google=failed`,
     session: false,
   }),
   (req, res) => {
